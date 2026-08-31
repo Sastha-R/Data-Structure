8827220ad7133c0f40201a24f8defaade0e9d9ab
Sastha <sastha00007@gmail.com>
2026-08-31 18:29:05 +0530

    trees added

---
+++ tree.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+
+
+class node{
+    public:
+        int data;
+        node* left;
+        node *right;
+
+    node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class tree{
+    public:
+        node *root;
+    tree(){
+        root = NULL;
+    }
+
+    void insert(){
+        root = new node(10);
+        root->left = new node(20);
+        root->right = new node(30);
+        root->left->left = new node(40);
+        root->left->right = new node(50);
+        root->right->left = new node(60);
+        root->right->right = new node(70);
+    }
+
+    void inorder(node *temp){
+        if(temp == NULL){
+            return;
+        }
+        inorder(temp->left);
+        cout<<temp->data<<" ";
+        inorder(temp->right);
+    }
+
+    void preorder(node *temp){
+        if(temp == NULL){
+            return;
+        }
+
+        cout<<temp->data<<" ";
+        preorder(temp->left);
+        preorder(temp->right);
+    }
+
+    void postorder(node *temp){
+        if(temp == NULL){
+            return;
+        }
+
+        postorder(temp->left);
+        postorder(temp->right);
+        cout<<temp->data<<" ";
+    }
+
+};
+
+int main(){ 
+
+    tree obj;
+    obj.insert();
+    cout<<"INORDER"<<endl;
+    obj.inorder(obj.root);
+    cout<<endl<<"PREORDER"<<endl;
+    obj.preorder(obj.root);
+    cout<<endl<<"POSTORDER"<<endl;
+    obj.postorder(obj.root);
+    return 0;
+}
